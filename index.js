2e779f6b5643caec2c45a6e6236d6aff30ae2c70
Elisa Vaccaro <erv230@nyu.edu>
2017-09-13 21:16:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(str){
 function logWhisper(str){
   console.log(str.toLowerCase());
 }
+
+function sayHiToGrandma(str){
+  if(str === whisper(str)){
+    return "I can't hear you!";
+  } else if (str === shout(str)){
+    return 'YES INDEED!';
+  } else if (str === 'I love you, Grandma.'){
+    return 'I love you, too.';
+  }
+}
